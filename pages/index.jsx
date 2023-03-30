ff4316ba93ab374349119385b17ef50c6512a4ba
Josh <2015JTW@gmail.com>
2023-03-30 14:05:07 -0400

    feat: edited dummy-data and implement getStaticProps for home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,28 @@
-import {getFeaturedEvents} from '../data/dummy-data'
 import EventList from '../components/events/event-list';
+import { getFeaturedEvents } from '../helpers/api-utils';
 
-export default function FeaturedEventsPage(){
-
-    const featuredEvents = getFeaturedEvents();
+export default function FeaturedEventsPage(props){
 
     return(
         <div>
             <h1>Featured Events Page</h1>
 
-            <EventList items={featuredEvents} />
+            <EventList items={props.featuredEvents} />
 
             
         </div>
     )
+}
+
+
+export async function getStaticProps(context){
+
+    const featuredEvents = await getFeaturedEvents();
+
+    return{
+        props: {
+            featuredEvents: featuredEvents
+        }
+    }
+
 }
