47234ddd6e2e8246292656291059391293fb439b
Josh <2015JTW@gmail.com>
2023-04-06 13:34:07 -0400

    fix: added getStaticProps to allEvents page and optimizaed it

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,12 +1,12 @@
-import { getAllEvents } from "../../data/dummy-data"
+import { getAllEvents } from "../../helpers/api-utils";
 import EventList from "../../components/events/event-list";
 import EventSearch from '../../components/events/event-search';
 import { Fragment } from "react";
 import {useRouter} from "next/router";
 
-export default function AllEventsPage(){
+export default function AllEventsPage(props){
     const router = useRouter();
-    const allEvents = getAllEvents();
+    const allEvents = props.allEvents;
 
     function findEventsHandler(selectedYear, selectedMonth){
 
@@ -20,4 +20,15 @@ export default function AllEventsPage(){
             <EventList items={allEvents} />
         </Fragment>
     )
+}
+
+export async function getStaticProps(){
+    const allEvents = await getAllEvents();
+
+    return{
+        props: {
+            allEvents: allEvents
+        },
+        revalidate: 60
+    }
 }
